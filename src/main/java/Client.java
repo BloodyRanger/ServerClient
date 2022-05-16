0b123e75959b9a30d488f819f045471f20ac6118
BloodyRanger <bloodyranger@inbox,ru>
2022-05-16 23:07:51 +0300

    Asterisk

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,22 +3,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
     public static void main(String[] args) {
-        String host = "localhost";
+        String host = "netology.homework";
         int port = 8089;
 
-        try {
-            Socket clientSocket = new Socket(host, port);
+        try (Socket clientSocket = new Socket(host, port);
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            Scanner sc = new Scanner(System.in)){
 
-            out.println("Client");
+            System.out.println(in.readLine());
+            String name = sc.nextLine();
+            out.println(name);
 
-            String resp = in.readLine();
-            System.out.println(resp);
+            System.out.println(in.readLine());
+            String childNot = sc.nextLine();
+            out.println(childNot);
+
+            System.out.println(in.readLine());
+
         } catch (IOException err) {
             err.printStackTrace();
         }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -11,14 +11,22 @@ public class Server {
         System.out.println("Server started");
         int port = 8089;
 
-        try {
-            ServerSocket serverSocket = new ServerSocket(port);
+        try (ServerSocket serverSocket = new ServerSocket(port);
             Socket clientSocket = serverSocket.accept();
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))){
 
-            final String name = in.readLine();
-            out.println(String.format("Hi, %s, your port is %d", name, clientSocket.getPort()));
+            out.println("Hi. What is your name?");
+            String name = in.readLine();
+
+            out.println("Are you child? (yes/no)");
+            String childNot = in.readLine();
+
+            if (childNot.equals("yes")) {
+                out.println("Welcome to the kids area, " + name + "! Let's play!");
+            } else {
+                out.println("Welcome to the adult zone, " + name + "! Have a good rest, or a good working day!");
+            }
         } catch (IOException err) {
             err.printStackTrace();
         }
